b77b0acb9d1aa22ce1a5fc530ef5d8589a2b0f2e
hannesrosenbusch <hannes.rosenbusch@gmail.com>
2024-09-10 12:59:45 +0200

    update everything

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+
 # Deep Learning In Python - Week 1
 
-<img src="https://github.com/deepLearningInPython/deeplearninginpythonclassroom-week1_python_pandas_numpy-DLIP_Week1_Python_Pandas_Numpy/blob/main/assets/uvalogo.svg.png?raw=true" width="125">    <img src="https://github.com/deepLearningInPython/deeplearninginpythonclassroom-week1_python_pandas_numpy-DLIP_Week1_Python_Pandas_Numpy/blob/main/assets/pythonlogo.jpeg?raw=true" width="380">
+<img src="https://github.com/deepLearningInPython/DLIP_WEEK1/blob/main/assets/uvalogo.svg.png?raw=true" width="125">    <img src="https://github.com/deepLearningInPython/DLIP_WEEK1/blob/main/assets/pythonlogo.jpeg?raw=true" width="380">
 
 
 An introduction to Python for R users can be seen here: https://youtube.com/playlist?list=PLq0cz82QvYapppmpXPYgS76VbHHKRIgbk&feature=shared
---
+++ week_1assignment copy.py
@@ -0,0 +1,182 @@
+print("hello world")
+# Follow the tasks below to practice basic Python concepts.
+# Write your code in between the dashed lines.
+
+# Task 1: Variable Assignment
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 2: Data Types
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 3: Conversions
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 4: Built-in Functions
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 5: String Manipulation
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 6: While Loops and For Loops
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 7: For Loops with Conditions
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 8: If-Else Statements
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 9: Nested If Statements
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 10: Combining For Loops and If-Else
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+# Task 11: 1D NumPy Arrays
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 12: 2D and 3D NumPy Arrays + Matrix Multiplication
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 13: Pandas DataFrames
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+# Task 14: Simulating Data and Adding to a DataFrame
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
+
+
+
+# Task 15: Looping and Selecting/Indexing in Pandas
+# Instructions:
+
+
+# Your code here:
+# -----------------------------------------------
+
+
+
+# -----------------------------------------------
